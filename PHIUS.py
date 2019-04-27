0803e58b052345acd07c7bd2a76713c29849a3bb
hamogu <moritz.guenther@gmx.de>
2019-04-26 21:48:16 -0400

    Fix missing coordinate, implement caching for locations

--- PHIUS.py
+++ PHIUS.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 
 from collections import OrderedDict
+import os
 import json
 import numpy as np
 
@@ -11,19 +12,22 @@ tab = Table.read('http://www.phius.org/phius-certification-for-buildings-product
 
 geolocator = Nominatim()
 
+if os.path.exists("coords_temp.json"):
+    with open("coords_temp.json", 'w') as f:
+        loc_by_hand = json.load(f)
 
-
-loc_by_hand = {'West Moberly, BC': {'coordinates': [-121.8553452, 55.8277321], 'type': 'Point'},
+else:
+    loc_by_hand = {'West Moberly, BC': {'coordinates': [-121.8553452, 55.8277321], 'type': 'Point'},
                'St. Joseph, IL': {'coordinates': [-88.0419502, 40.1136387], 'type': 'Point'},
                'Unicorporated Adams County, CO': {'coordinates': [-104.9785223, 39.9212746], 'type': 'Point'},
                'Yokohama City, Japan': {'coordinates': [139.5490381, 35.4619297], 'type': 'Point'},
                'Va. Beach, VA': {'coordinates': [-76.2935035, 36.7953392], 'type': 'Point'},
                'Elk, WA': {'coordinates': [-117.2853794, 48.0162853], 'type': 'Point'},
-               'Lake Geneva (Jainsville/Rock County, WI climate data set), WI': {},
+               'Lake Geneva (Jainsville/Rock County, WI climate data set), WI': {'coordinates': [-88.4509902, 42.5866519], 'type': 'Point'},
                'Belingham, WA':  {'coordinates': [-122.5313497, 48.753404], 'type': 'Point'},
                'Wurtsburo, NY':  {'coordinates': [-74.4945967, 41.5765227], 'type': 'Point'},
                'Shaw Island, San Juan Islands, WA':  {'coordinates': [-122.9949629, 48.570948], 'type': 'Point'},
-}
+    }
 
 
 out = {'type': "FeatureCollection", 'features': []}
@@ -71,6 +75,10 @@ for row in tab:
     out['features'].append({"type": "Feature",
                             "geometry": loc,
                             "properties": prop})
+
+# Save so I don't need to get all the data from th eInternet next time, only the new locations.
+with open("coords_temp.json", 'w') as f:
+    json.dump(loc_by_hand, f)
 
 
 with open("PHIUS.geojson", 'w') as f:
